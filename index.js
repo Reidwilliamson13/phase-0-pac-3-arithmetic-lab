59fa8fdf5036d77b65eec87bdc131c39d9cc2c12
ReidWilliamson <reidwilliamson2@gmail.com>
2021-08-20 17:24:29 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,75 @@
+function add() {
+  return 1076 + 1;
+}
+console.log(add());
+
+function subtract() {
+  return 5 - 3;
+}
+console.log(subtract());
+
+function divide() {
+  return 120 / 10;
+}
+console.log(divide());
+
+function multiply() {
+  return 5 * 6;
+}
+console.log(multiply());
+
+function add(a, b) {
+  return;
+}
+let a = 1058;
+let b = 1;
+function add(a, b) {
+  return a + b;
+}
+console.log(add(a, b));
+
+let ax = 285;
+let bx = 2;
+function subtract(bx, ax) {
+  return bx - ax;
+}
+console.log(subtract(ax, bx));
+
+let ab = 437656;
+let ac = 2;
+function multiply(ab, ac) {
+  return ab * ac;
+}
+console.log(multiply(ab, ac));
+
+let cx = 1.36734693878;
+let dx = 2;
+function multiply(cx, dx) {
+  return cx * dx;
+}
+console.log(multiply(cx, dx));
+
+let ee = 12;
+let rr = 6;
+function divide(ee, rr) {
+  return ee / rr;
+}
+console.log(divide(ee, rr));
+
+function increment(n) {
+  n += 1;
+  return n;
+}
+
+function decrement(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
